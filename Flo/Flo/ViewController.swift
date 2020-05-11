184817aba4c989e1c4cd4b616ebf9545f2ec7b73
Zack Zhang <zack.zhang@salmet.de>
2020-05-11 15:26:35 +0800

    first commit

---
+++ Flo/Flo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Flo
+//
+//  Created by Zack Zhang on 2020/3/23.
+//  Copyright © 2020 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print((2<<3)*(2<<2))
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
